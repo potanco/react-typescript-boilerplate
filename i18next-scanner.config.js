81405b17aa0b04b2ceb9f442b74d7fef93d6bbb3
Akar Muhamad <akarmuhamad13@gmail.com>
2021-07-28 16:35:05 +0300

    Add Eslint and Husky hooks

--- i18next-scanner.config.js
+++ i18next-scanner.config.js
@@ -1,54 +1,54 @@
-const fs = require("fs");
-const chalk = require("chalk");
+const fs = require('fs');
+const chalk = require('chalk');
 
 module.exports = {
   input: [
-    "src/**/*.{js,jsx,ts,tsx}",
+    'src/**/*.{js,jsx,ts,tsx}',
     // Use ! to filter out files or directories
-    "!src/**/*.spec.{js,jsx,ts,tsx}",
-    "!src/i18n/**",
-    "!**/node_modules/**",
+    '!src/**/*.spec.{js,jsx,ts,tsx}',
+    '!src/i18n/**',
+    '!**/node_modules/**',
   ],
-  output: "./",
+  output: './',
   options: {
     debug: true,
     func: {
-      list: ["i18next.t", "i18n.t", "t"],
-      extensions: [".js", ".jsx", ".ts", ".tsx"],
+      list: ['i18next.t', 'i18n.t', 't'],
+      extensions: ['.js', '.jsx', '.ts', '.tsx'],
     },
     trans: {
-      component: "Trans",
-      i18nKey: "i18nKey",
-      defaultsKey: "defaults",
-      extensions: [".js", ".jsx", ".ts", ".tsx"],
+      component: 'Trans',
+      i18nKey: 'i18nKey',
+      defaultsKey: 'defaults',
+      extensions: ['.js', '.jsx', '.ts', '.tsx'],
       fallbackKey: function (ns, value) {
         return value;
       },
       acorn: {
         ecmaVersion: 10, // defaults to 10
-        sourceType: "module", // defaults to 'module'
+        sourceType: 'module', // defaults to 'module'
         // Check out https://github.com/acornjs/acorn/tree/master/acorn#interface for additional options
       },
     },
-    lngs: ["en", "ar", "ku"],
-    ns: ["common", "validations"],
-    defaultNS: "common",
-    fallbackLng: "en",
+    lngs: ['en', 'ar', 'ku'],
+    ns: ['common', 'validations'],
+    defaultNS: 'common',
+    fallbackLng: 'en',
 
-    defaultLng: "en",
-    defaultNs: "common",
+    defaultLng: 'en',
+    defaultNs: 'common',
     defaultValue: function (lng, ns, key) {
       return key;
     },
     resource: {
-      loadPath: "./public/locales/{{lng}}/{{ns}}.json",
-      savePath: ".//public/locales/{{lng}}/{{ns}}.json",
+      loadPath: './public/locales/{{lng}}/{{ns}}.json',
+      savePath: './/public/locales/{{lng}}/{{ns}}.json',
       jsonIndent: 2,
-      lineEnding: "\n",
+      lineEnding: '\n',
     },
     interpolation: {
-      prefix: "{{",
-      suffix: "}}",
+      prefix: '{{',
+      suffix: '}}',
     },
   },
   transform: function customTransform(file, enc, done) {
@@ -56,27 +56,19 @@ module.exports = {
     const content = fs.readFileSync(file.path, enc);
     let count = 0;
 
-    parser.parseFuncFromString(
-      content,
-      { list: ["i18next._", "i18next.__"] },
-      (key, options) => {
-        parser.set(
-          key,
-          Object.assign({}, options, {
-            nsSeparator: false,
-            keySeparator: false,
-          })
-        );
-        ++count;
-      }
-    );
+    parser.parseFuncFromString(content, { list: ['i18next._', 'i18next.__'] }, (key, options) => {
+      parser.set(
+        key,
+        Object.assign({}, options, {
+          nsSeparator: false,
+          keySeparator: false,
+        }),
+      );
+      ++count;
+    });
 
     if (count > 0) {
-      console.log(
-        `i18next-scanner: count=${chalk.cyan(count)}, file=${chalk.yellow(
-          JSON.stringify(file.relative)
-        )}`
-      );
+      console.log(`i18next-scanner: count=${chalk.cyan(count)}, file=${chalk.yellow(JSON.stringify(file.relative))}`);
     }
 
     done();
--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,15 @@
-import React, { Suspense } from "react";
-import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
-import App from "./app/App";
-import store from "./app/store";
-import "./i18n";
-import "./index.css";
-import reportWebVitals from "./reportWebVitals";
+import React, { Suspense } from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import App from './app/App';
+import store from './app/store';
+import './i18n';
+import './index.css';
+import reportWebVitals from './reportWebVitals';
 
-export const lang = localStorage.getItem("lang")
-  ? localStorage.getItem("lang")
-  : localStorage.setItem("lang", "en");
+export const lang = localStorage.getItem('lang') ? localStorage.getItem('lang') : localStorage.setItem('lang', 'en');
 
-export const isRtl = lang !== undefined ? lang !== "en" : false;
+export const isRtl = lang !== undefined ? lang !== 'en' : false;
 
 ReactDOM.render(
   <React.StrictMode>
@@ -23,7 +21,7 @@ ReactDOM.render(
       )}
     </Provider>
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root'),
 );
 
 // If you want to start measuring performance in your app, pass a function
